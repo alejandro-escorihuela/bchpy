2b223088cc2cf7309deb7be4264dbb385aff1dd0
alescori <cosmogatespacial@gmail.com>
2020-12-04 15:51:06 +0100

    basic corxet

--- bchpy.py
+++ bchpy.py
@@ -12,32 +12,44 @@ from sympy.printing.pretty.pretty_symbology import pretty_symbol
 
 class Eel(sp.Expr):
     is_commutative = False
+    is_number = False
+    is_zero = False
     def __init__(self, i = 1, j = 1):
         self.i = i
         self.j = j
-        
+        if i == 0:
+            self.is_zero = True
+            
     def _sympystr(self, printer, *args):
+        if self.is_zero:
+            return "0"
         return "E%s%s" % (printer._print(self.args[0]), printer._print(self.args[1]))
     
     def _pretty(self, printer, *args):
+        if self.is_zero:
+            return prettyForm(pretty_symbol("0"))
         return prettyForm(pretty_symbol("E_%s%s" % (printer._print(self.args[0]), printer._print(self.args[1]))))
 
 class Corxet(sp.Expr):
-    is_commutative = False
     def __new__(cls, A, B):
+        return cls.eval(cls, A, B)
+
+    def eval(cls, A, B):
+        if A.compare(B) == 1:
+            return sp.S.NegativeOne*cls.eval(cls, B, A)
         i, j, k, l = A.i, A.j, B.i, B.j
-        l = rl.relE[i][j][k][l]
+        l = rl.corxetErel(i, j, k, l)
         args = []
         for it in l:
-            elem = sp.Mul(sp.S(it[0]), Eel(it[1], it[2]))
+            elem = sp.Mul(sp.Rational(it[0], it[1]), Eel(it[2], it[3]))
             args.append(elem)
-        print(sp.srepr(sp.Add(*args)))
         return sp.Add(*args)
-    
+
 if __name__ == "__main__":
+    print(Eel(0,0))
     E11=Eel(1, 1)
-    E42=Eel(4, 2)
+    E62=Eel(6, 2)
     E21=Eel(2, 1)
     print(Corxet(E11, E21))
-    ec=Corxet(E21, E42)
+    ec=Corxet(Eel(7,1), Eel(3,4))
     print(ec)
