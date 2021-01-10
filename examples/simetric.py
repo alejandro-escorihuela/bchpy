5d52c82e1500d14ce0adf3eae339fb07994d75da
cosmogat <cosmogatespacial@gmail.com>
2021-01-10 23:33:24 +0100

    move example file

--- examples/simetric.py
+++ examples/simetric.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*- 
 # 09-12-2020
 # alex
-# s64.py
+# simetric.py
 
 import numpy as np
 import sympy as sp
@@ -16,9 +16,10 @@ if __name__ == "__main__":
     print("s64")
     a = sp.symbols("a0:3")
     b = sp.symbols("b0:4")
-    
+
     t0 = tm.time()
     met = Metode(5)
+    met.setBAB(b[0], a[0], b[0])
     met.setABA(b[0], a[0], b[1], a[1], b[2], sp.Rational(1, 2) - a[1] - a[0], sp.S(1) - (sp.S(2)*(b[0] + b[1] + b[2])), sp.Rational(1, 2) - a[1] - a[0], b[2], a[1], b[1], a[0], b[0], debug=True)
     # met.setABA(sp.S(0.0792036964311956), sp.S(0.2095151066133620), sp.S(0.3531729060497740), sp.S(-0.143851773179818), sp.S(-0.042065080357719), sp.S(0.434336666566456), sp.S(0.2193769557534987), sp.S(0.434336666566456), sp.S(-0.042065080357719), sp.S(-0.143851773179818), sp.S(0.3531729060497740), sp.S(0.2095151066133620), sp.S(0.0792036964311956), debug=True)
     met.cprint()
