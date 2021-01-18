634083090bda53b4bd3cde74f1cf5b9e8d473757
cosmogat <cosmogatespacial@gmail.com>
2021-01-18 07:41:41 +0100

    monday

--- examples/recursives.py
+++ examples/recursives.py
@@ -14,7 +14,7 @@ import relations as rl
 
 if __name__ == "__main__":
     ord_bch = 9
-    # print("exp(bet(i,j)*Eij)=exp(x*E12)*exp(alp(i,j)*Eij)")
+    # print("exp(w(i,j)*Eij)=exp(x*E12)*exp(alp(i,j)*Eij)")
     # x, y = sp.symbols("x y")
     # t0 = tm.time()
     # esq = sp.S(0)
@@ -29,17 +29,69 @@ if __name__ == "__main__":
     # t1 = tm.time()
     # print(t1 - t0, "s")
     
-    print("exp(gam(i, j)*Eij)=exp(y*E11)*exp(alp(i, j)*Eij)")
-    x = sp.Symbol("x")
+    # print("exp(w(i, j)*Eij)=exp(y*E11)*exp(alp(i, j)*Eij)")
+    # x = sp.Symbol("x")
+    # t0 = tm.time()
+    # esq = sp.S(0)
+    # alp = sp.MatrixSymbol("alp", ord_bch + 1, rl.tamE[ord_bch - 1] + 1)
+    # for i in range(ord_bch):
+    #     for j in range(rl.tamE[i]):
+    #         esq += alp[i + 1, j + 1]*Eel(i + 1, j + 1)
+    # esq = bch9(x*Eel(1, 1), esq, depth = ord_bch, debug = True)
+    # metAD = Metode(ord_bch)
+    # metAD.importFromExpr(esq, debug = True)
+    # print(metAD)
+    # t1 = tm.time()
+    # print(t1 - t0, "s")
+
+    # print("exp(w(i,j)*Eij)=exp(x*E12)*exp(alp(i,j)*Eij)*exp(x*E12)")
+    # x, y = sp.symbols("x y")
+    # t0 = tm.time()
+    # esq = sp.S(0)
+    # alp = sp.MatrixSymbol("alp", ord_bch + 1, rl.tamE[ord_bch - 1] + 1)
+    # for i in range(0, ord_bch, 2):
+    #     for j in range(rl.tamE[i]):
+    #         esq += alp[i + 1, j + 1]*Eel(i + 1, j + 1)
+    # esq = bch9(esq, x*Eel(1, 2), depth = ord_bch, debug = True)
+    # esq = bch9(x*Eel(1, 2), esq.expand(), depth = ord_bch, debug = True)
+    # metBDB = Metode(ord_bch)
+    # metBDB.importFromExpr(esq, debug = True)
+    # print(metBDB)
+    # t1 = tm.time()
+    # print(t1 - t0, "s")  
+    
+    # print("exp(w(i,j)*Eij)=exp(x*E11)*exp(alp(i,j)*Eij)*exp(x*E11)")
+    # x, y = sp.symbols("x y")
+    # t0 = tm.time()
+    # esq = sp.S(0)
+    # alp = sp.MatrixSymbol("alp", ord_bch + 1, rl.tamE[ord_bch - 1] + 1)
+    # for i in range(0, ord_bch, 2):
+    #     for j in range(rl.tamE[i]):
+    #         esq += alp[i + 1, j + 1]*Eel(i + 1, j + 1)
+    # esq = bch9(esq, x*Eel(1, 1), depth = ord_bch, debug = True)
+    # esq = bch9(x*Eel(1, 1), esq.expand(), depth = ord_bch, debug = True)
+    # metBDB = Metode(ord_bch)
+    # metBDB.importFromExpr(esq, debug = True)
+    # print(metBDB)
+    # t1 = tm.time()
+    # print(t1 - t0, "s") 
+
+    print("exp(w(i,j)*Eij)=exp(x*E12)*exp(alp(i,j)*Eij)*exp(x.conjugate()*E12)")
+    x, y = sp.symbols("x y")
     t0 = tm.time()
     esq = sp.S(0)
     alp = sp.MatrixSymbol("alp", ord_bch + 1, rl.tamE[ord_bch - 1] + 1)
     for i in range(ord_bch):
         for j in range(rl.tamE[i]):
-            esq += alp[i + 1, j + 1]*Eel(i + 1, j + 1)
-    esq = bch9(x*Eel(1, 1), esq, depth = ord_bch, debug = True)
-    metAD = Metode(ord_bch)
-    metAD.importFromExpr(esq, debug = True)
-    print(metAD)
+            if (i + 1) % 2 == 0:
+                esq += sp.I*alp[i + 1, j + 1]*Eel(i + 1, j + 1)
+            else:
+                esq += alp[i + 1, j + 1]*Eel(i + 1, j + 1)
+    esq = bch9(esq, x.conjugate().expand(complex=True)*Eel(1, 2), depth = ord_bch, debug = True)
+    esq = bch9(x.expand(complex=True)*Eel(1, 2), esq.expand(), depth = ord_bch, debug = True)
+    metBCB = Metode(ord_bch)
+    metBCB.importFromExpr(esq, debug = True)
+    metBCB.collectI()
+    print(metBCB)
     t1 = tm.time()
-    print(t1 - t0, "s")
+    print(t1 - t0, "s")    
