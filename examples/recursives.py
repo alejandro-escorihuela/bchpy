a3c8f691650645d7f447ad7d2e724324254685ad
alescori <cosmogatespacial@gmail.com>
2021-01-19 08:43:55 +0100

    add symmetric ACA bch calculation

--- examples/recursives.py
+++ examples/recursives.py
@@ -76,7 +76,29 @@ if __name__ == "__main__":
     # t1 = tm.time()
     # print(t1 - t0, "s") 
 
-    print("exp(w(i,j)*Eij)=exp(x*E12)*exp(alp(i,j)*Eij)*exp(x.conjugate()*E12)")
+    # print("exp(w(i,j)*Eij)=exp(x*E12)*exp(alp(i,j)*Eij)*exp(x.conjugate()*E12)")
+    # x, y = sp.symbols("x y")
+    # t0 = tm.time()
+    # esq = sp.S(0)
+    # alp = sp.MatrixSymbol("alp", ord_bch + 1, rl.tamE[ord_bch - 1] + 1)
+    # for i in range(ord_bch):
+    #     for j in range(rl.tamE[i]):
+    #         if (i + 1) % 2 == 0:
+    #             esq += sp.I*alp[i + 1, j + 1]*Eel(i + 1, j + 1)
+    #         else:
+    #             esq += alp[i + 1, j + 1]*Eel(i + 1, j + 1)
+    # esq = bch9(esq, x.conjugate().expand(complex=True)*Eel(1, 2), depth = ord_bch, debug = True)
+    # esq = collectEsq(esq)
+    # esq = bch9(x.expand(complex=True)*Eel(1, 2), esq, depth = ord_bch, debug = True)
+    # metBCB = Metode(ord_bch)
+    # metBCB.importFromExpr(esq, debug = True)
+    # metBCB.collectI()
+    # print(metBCB)
+    # t1 = tm.time()
+    # print(t1 - t0, "s")  
+    
+
+    print("exp(w(i,j)*Eij)=exp(x*E11)*exp(alp(i,j)*Eij)*exp(x.conjugate()*E11)")
     x, y = sp.symbols("x y")
     t0 = tm.time()
     esq = sp.S(0)
@@ -87,12 +109,12 @@ if __name__ == "__main__":
                 esq += sp.I*alp[i + 1, j + 1]*Eel(i + 1, j + 1)
             else:
                 esq += alp[i + 1, j + 1]*Eel(i + 1, j + 1)
-    esq = bch9(esq, x.conjugate().expand(complex=True)*Eel(1, 2), depth = ord_bch, debug = True)
+    esq = bch9(esq, x.conjugate().expand(complex=True)*Eel(1, 1), depth = ord_bch, debug = True)
     esq = collectEsq(esq)
-    esq = bch9(x.expand(complex=True)*Eel(1, 2), esq, depth = ord_bch, debug = True)
-    metBCB = Metode(ord_bch)
-    metBCB.importFromExpr(esq, debug = True)
-    metBCB.collectI()
-    print(metBCB)
+    esq = bch9(x.expand(complex=True)*Eel(1, 1), esq, depth = ord_bch, debug = True)
+    metACA = Metode(ord_bch)
+    metACA.importFromExpr(esq, debug = True)
+    metACA.collectI()
+    print(metACA)
     t1 = tm.time()
-    print(t1 - t0, "s")    
+    print(t1 - t0, "s")       
