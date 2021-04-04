d02195116c235084f45ea0e0b9c4b25fe485f536
alescori <cosmogatespacial@gmail.com>
2021-04-04 18:53:43 +0200

    some changes

--- examples/simetric.py
+++ examples/simetric.py
@@ -20,23 +20,27 @@ if __name__ == "__main__":
     t0 = tm.time()
     met = Metode(7)
  
-    met.setBAB(b[0], a[0], b[1], a[1], b[2], a[2], b[3], a[3], b[4], a[4], b[5], a[5], b[6], a[6], b[7], a[7], b[8], a[8], b[9], a[9], b[10], a[10], b[11], a[11], b[12], a[12], b[13], a[13], b[14], a[13], b[13], a[12], b[12], a[11], b[11], a[10], b[10], a[9], b[9], a[8], b[8], a[7], b[7], a[6], b[6], a[5], b[5], a[4], b[4], a[3], b[3], a[2], b[2], a[1], b[1], a[0], b[0], debug=True)
-   
+    #met.setABA(a[0], b[0], a[1], b[1], a[2], b[2], a[3], b[3], a[4], sp.Rational(1, 2) - (b[0] + b[1] + b[2] + b[3]), 1 - 2*(a[0] + a[1] + a[2] + a[3] + a[4]), sp.Rational(1, 2) - (b[0] + b[1] + b[2] + b[3]), a[4], b[3], a[3], b[2], a[2], b[1], a[1], b[0], a[0], debug=True)
+    # a13, b13 = sp.symbols("a13 b13")
+    # a13 = sp.Rational(1, 2) - (a[0] + a[1] + a[2] + a[3] + a[4] + a[5] + a[6] + a[7] + a[8] + a[9] + a[10] + a[11] + a[12])
+    # b13 = 1 - 2*(b[0] + b[1] + b[2] + b[3] + b[4] + b[5] + b[6] + b[7] + b[8] + b[9] + b[10] + b[11] + b[12])
+    met.setABA(a[0], b[0], a[1], b[1], a[2], b[2], a[3], b[3], a[4], b[4], a[5], b[5], a[6], b[6], a[7], b[7], a[8], b[8], a[9], b[9], a[10], b[10], a[11], b[11], a[12], b[12], a[13], b[13], a[13], b[12], a[12], b[11], a[11], b[10], a[10], b[9], a[9], b[8], a[8], b[7], a[7], b[6], a[6], b[5], a[5], b[4], a[4], b[3], a[3], b[2], a[2], b[1], a[1], b[0], a[0], debug=True)  
     t1 = tm.time()
     print(t1 - t0, "s")
 
     printi("Expandint mètode")
-    met.expand(debug = True)
+    #met.expand(debug = True)
     t2 = tm.time()
     print(t2 - t1, "s")
 
     # printi("Desant fitxer amb el mètode")
-    # met.save("sim8_27.dat")
+    # met.save("sim6_10.dat")
     # t3 = tm.time()
     # print(t3 - t2, "s")
 
     printi("Exportant equacions a python")
     met.exportpy("sim8_27.py")
     t2 = tm.time()
-    print(t2 - t1, "s")    
+    print(t2 - t1, "s")
+    
 
