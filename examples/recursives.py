5b20bb520a96d49e63adee3111942a29a8c05e7a
cosmogat <cosmogatespacial@gmail.com>
2022-09-29 12:29:27 +0200

    recursive calculation prepared for symconj

--- examples/recursives.py
+++ examples/recursives.py
@@ -136,31 +136,7 @@ if __name__ == "__main__":
     # t1 = tm.time()
     # print(t1 - t0, "s") 
 
-    # print("exp(w(i,j)*Eij)=exp(x*E12)*exp(alp(i,j)*Eij)*exp(x.conjugate()*E12)")
-    # x, y, xre, xim = sp.symbols("x y xre xim")
-    # t0 = tm.time()
-    # esq = sp.S(0)
-    # alp = sp.MatrixSymbol("alp", ord_bch + 1, rl.tamE[ord_bch - 1] + 1)
-    # for i in range(ord_bch):
-    #     for j in range(rl.tamE[i]):
-    #         if (i + 1) % 2 == 0:
-    #             esq += sp.I*alp[i + 1, j + 1]*Eel(i + 1, j + 1, "E")
-    #         else:
-    #             esq += alp[i + 1, j + 1]*Eel(i + 1, j + 1, "E")
-    # esq = bch9(esq, x.conjugate().expand(complex=True)*Eel(1, 2, "E"), depth = ord_bch, debug = True)
-    # esq = collectEsq(esq)
-    # esq = bch9(x.expand(complex=True)*Eel(1, 2, "E"), esq, depth = ord_bch, debug = True)
-    # metBCB = Metode(ord_bch)
-    # metBCB.importFromExpr(esq, debug = True)
-    # metBCB.collectI()
-    # # print(metBCB)
-    # printmetC(metBCB, ord_bch, simconj = True)
-    # t1 = tm.time()
-    # print(t1 - t0, "s") 
-    
-    
-
-    print("exp(w(i,j)*Eij)=exp(x*E11)*exp(alp(i,j)*Eij)*exp(x.conjugate()*E11)")
+    print("exp(w(i,j)*Eij)=exp(x*E12)*exp(alp(i,j)*Eij)*exp(x.conjugate()*E12)")
     x, y, xre, xim = sp.symbols("x y xre xim")
     t0 = tm.time()
     esq = sp.S(0)
@@ -171,13 +147,38 @@ if __name__ == "__main__":
                 esq += sp.I*alp[i + 1, j + 1]*Eel(i + 1, j + 1, "E")
             else:
                 esq += alp[i + 1, j + 1]*Eel(i + 1, j + 1, "E")
-    esq = bch9(esq, x.conjugate().expand(complex=True)*Eel(1, 1, "E"), depth = ord_bch, debug = True)
+    esq = bch9(esq, x.conjugate().expand(complex=True)*Eel(1, 2, "E"), depth = ord_bch, debug = True)
     esq = collectEsq(esq)
-    esq = bch9(x.expand(complex=True)*Eel(1, 1, "E"), esq, depth = ord_bch, debug = True)
-    metACA = Metode(ord_bch)
-    metACA.importFromExpr(esq, debug = True)
-    metACA.collectI()
-    # print(metACA)
-    printmetC(metACA, ord_bch, simconj = True)    
+    esq = bch9(x.expand(complex=True)*Eel(1, 2, "E"), esq, depth = ord_bch, debug = True)
+    metBCB = Metode(ord_bch)
+    metBCB.importFromExpr(esq, debug = True)
+    metBCB.collectI()
+    # print(metBCB)
+    printmetC(metBCB, ord_bch, simconj = True)
     t1 = tm.time()
-    print(t1 - t0, "s")       
+    print(t1 - t0, "s") 
+    
+    
+
+    # print("exp(w(i,j)*Eij)=exp(x*E11)*exp(alp(i,j)*Eij)*exp(x.conjugate()*E11)")
+    # x, y, xre, xim = sp.symbols("x y xre xim")
+    # t0 = tm.time()
+    # esq = sp.S(0)
+    # alp = sp.MatrixSymbol("alp", ord_bch + 1, rl.tamE[ord_bch - 1] + 1)
+    # for i in range(ord_bch):
+    #     for j in range(rl.tamE[i]):
+    #         if (i + 1) % 2 == 0:
+    #             esq += sp.I*alp[i + 1, j + 1]*Eel(i + 1, j + 1, "E")
+    #         else:
+    #             esq += alp[i + 1, j + 1]*Eel(i + 1, j + 1, "E")
+    # esq = bch9(esq, x.conjugate().expand(complex=True)*Eel(1, 1, "E"), depth = ord_bch, debug = True)
+    # esq = collectEsq(esq)
+    # esq = bch9(x.expand(complex=True)*Eel(1, 1, "E"), esq, depth = ord_bch, debug = True)
+    # metACA = Metode(ord_bch)
+    # metACA.importFromExpr(esq, debug = True)
+    # metACA.collectI()
+    # # print(metACA)
+    # printmetC(metACA, ord_bch, simconj = True)    
+    # t1 = tm.time()
+    # print(t1 - t0, "s")   
+    
